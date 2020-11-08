fa67c507bd204ca89346152dbe97079444201c65
mattwells19 <mwells21@uncc.edu>
2020-11-08 13:51:29 -0500

    Try typescript

---
+++ index.ts
@@ -0,0 +1,25 @@
+import { Client, Message } from "discord.js";
+import { config } from "dotenv";
+
+config();
+const client = new Client({});
+
+client.on("ready", async () => {
+  console.log("QueuePop is logged in.");
+});
+
+client.on("message", async (msg: Message) => {
+  if (!msg.content.startsWith("-") || msg.author.bot || !msg.guild) return;
+
+  msg.channel.startTyping();
+
+  // remove prefix from message
+  const command = msg.content.slice(1).trim().split(" ")[0].toLowerCase();
+  const guildId: string = msg.guild.id;
+
+  if (command === "team") msg.channel.send("Hi");
+
+  await msg.channel.stopTyping();
+});
+
+client.login(process.env.DISCORD_TOKEN);
